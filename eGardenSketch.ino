d3f2b2195744243c8174a4154a33f1905437d52e
deJames-13 <de.james013@gmail.com>
2024-03-20 05:05:22 +0800

    dsf

--- eGardenSketch.ino
+++ eGardenSketch.ino
@@ -1,53 +1,160 @@
 #include "includes.h"
 
-DHT22SENSOR dhtSensor;
-SoilMoisture soilSensor;
-WaterLevel waterSensor;
+// WEB
 OLED oledDisplay;
 MyWebServer webServer;
+NetworkManager networkManager; // FIXME: Network
+DatabaseManager dbManager;     // FIXME: Database
+
+// SENSORS
+DHTSENSOR dhtSensor(DHT_PIN, DHTTYPE);
+SoilMoisture soilSensor(SOIL_MOISTURE_PIN);
+WaterLevel waterSensor(WATER_LEVEL_PIN);
+
+// OUTPUT
+Buzzer buzzer(BUZZER_PIN);
+Fan fan1(RELAY_PIN_FAN1);
+Fan fan2(RELAY_PIN_FAN2);
+WaterValve valve(RELAY_PIN_VALVE);
+
+// ACTIONS
+void handleWatering(String waterLevel);
+void handleFan(Fan fan);
+void handleBuzzer();
+
+// TESTS
+void handleButtonClick();
+int buttonPin = 4;
+int buttonState = 0;
 
 void setup()
 {
-    Serial.begin(9600);
-
+    Serial.begin(4800);
     dhtSensor.begin();
     soilSensor.begin();
     waterSensor.begin();
-    oledDisplay.begin();
+
+    fan1.begin();
+    fan2.begin();
+    buzzer.begin();
+    valve.begin();
+
+    oledDisplay.begin(); // COMMENT OUT ON UR OWN RISK
+    networkManager.connectToWiFi(WIFI_SSID, WIFI_PASSWORD);
     webServer.begin();
+
+    // TESTS
+    pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop()
 {
+    delay(500);
+    // Serial.println("looping");
+
     // #########################################################
-    // SENSOR INPUTS        FIXME: Test sensor inputs
+    // SENSOR INPUTS
     // #########################################################
-    float temperature = dhtSensor.getTemperature();
-    float humidity = dhtSensor.getHumidity();
-    int moisture = soilSensor.getMoisture();
-    String waterLevel = waterSensor.getWaterLevel();
-    int waterValue = waterSensor.getSensorValue();
+    // FIXME: Test new DHT SENSOR
+    // float temperature = dhtSensor.getTemperature();
+    // float humidity = dhtSensor.getHumidity();
+    // Serial.print("Temperature: ");
+    // Serial.println(temperature);
+    // Serial.print("Humidity: ");
+    // Serial.println(humidity);
+    // int moisture = soilSensor.getMoisture();
+    // String waterLevel = waterSensor.getWaterLevel();
+    // int waterValue = waterSensor.getSensorValue();
+    // #########################################################
+
+    // #########################################################
+    // OLED INFO DISPLAY    FIXME: Adjsut Display
+    // Update display with sensor data
+    // #########################################################
+    // oledDisplay.clearDisplay();
+    // oledDisplay.displayTemperature(temperature);
+    // oledDisplay.displayHumidity(humidity);
+    // oledDisplay.displayMoisture(moisture);
+    // oledDisplay.displayWaterStatus(waterLevel, waterValue);
+    // oledDisplay.updateDisplay();
+
+    // #########################################################
+    // OUTPUT EVENTS
+    // Update what will happen to output components: buzzer, fan, valve
+    // #########################################################
+    // handleBuzzer();
+    // handleWatering(waterLevel);
+    // handleFan(fan1);
     // #########################################################
 
     // #########################################################
     // WEB SERVER           FIXME: Test web server
     // Update sensor data on web server
     // #########################################################
-    webServer.updateSensorData(temperature, humidity, moisture, waterLevel, waterValue);
-    // #########################################################
-
-    // #########################################################
-    // OlED INFO DISPLAY    FIXME: Test OLED display
-    // Update display with sensor data
-    // #########################################################
-    oledDisplay.clearDisplay();
-    oledDisplay.displayTemperature(temperature);
-    oledDisplay.displayHumidity(humidity);
-    oledDisplay.displayMoisture(moisture);
-    oledDisplay.displayWaterStatus(waterLevel, waterValue);
-    oledDisplay.updateDisplay();
-
+    // webServer.updateSensorData(temperature, humidity, moisture, waterLevel, waterValue);
     // HANDLE CLIENT SERVER
     webServer.handleClient();
-    delay(1000);
+    // #########################################################
+
+    // TESTING
+    // handleButtonClick();
+}
+
+// #########################################################
+// ACTION EVENTS
+// #########################################################
+
+void handleWatering(String waterLevel)
+{
+    // FIXME: TEST LOGIC
+    if (soilSensor.isTooDry() && waterLevel != "LOW")
+    {
+        valve.open();
+    }
+    else if (soilSensor.isTooWet())
+    {
+        valve.close();
+    }
+}
+
+void handleFan(Fan fan)
+{
+    // FIXME: TEST LOGIC
+    const float temperatureThreshold = 30;
+    if (dhtSensor.isHot(temperatureThreshold))
+    {
+        fan.turnOn();
+    }
+    else
+    {
+        fan.turnOff();
+    }
+}
+
+void handleBuzzer()
+{
+    // FIXME: TEST LOGIC
+    if (waterSensor.isEmptyOrLow())
+    {
+        buzzer.buzzOnce();
+        buzzer.setBuzzState(true);
+    }
+    else
+    {
+        buzzer.setBuzzState(false);
+    }
+}
+
+void handleButtonClick()
+{
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH)
+    {
+        Serial.println("clicked");
+        fan1.turnOn();
+    }
+    else
+    {
+        fan1.turnOff();
+    }
 }
